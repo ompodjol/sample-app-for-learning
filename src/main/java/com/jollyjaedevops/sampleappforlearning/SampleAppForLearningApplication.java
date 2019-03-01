00628de50c4fd895c1a90b6634a35029aadd8785
Joffry Ferrater <joffry.ferrater@axiomatics.com>
2019-03-01 19:36:55 +0100

    project setup

---
+++ src/main/java/com/jollyjaedevops/sampleappforlearning/SampleAppForLearningApplication.java
@@ -0,0 +1,13 @@
+package com.jollyjaedevops.sampleappforlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppForLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppForLearningApplication.class, args);
+	}
+
+}
